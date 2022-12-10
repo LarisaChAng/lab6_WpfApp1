3076b1d52e8313950ebd826aff70271b66d84227
LarisaChAng <LChS81@mail.ru>
2022-12-11 01:00:12 +0800

    Добавьте файлы проекта.

---
+++ lab6_WpfApp1/WeatherControl.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace lab6_WpfApp1
+{
+    public enum Precipitation
+    {
+        sunny,
+        cloudy,
+        rain,
+        snow
+    }
+
+    class WeatherControl : DependencyObject
+    {
+        private Precipitation precipitationW;
+        public string w_dir;
+        private int w_sp;
+
+        public string WDir { get; set; }
+        public int WSp { get; set; }
+
+        public WeatherControl(string winddir, int windsp, Precipitation precipitation)
+        {
+            this.WDir = winddir;
+            this.WSp = windsp;
+            this.precipitationW = precipitation;
+        }
+        public static readonly DependencyProperty TempProperty;
+        public int Temp
+        {
+            get => (int)GetValue(TempProperty);
+            set => SetValue(TempProperty, value);
+        }
+
+        static WeatherControl()
+        {
+            TempProperty = DependencyProperty.Register(
+                nameof(Temp),
+                typeof(int),
+                typeof(WeatherControl),
+                new FrameworkPropertyMetadata(
+                    0,
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.AffectsRender,
+                    null,
+                    new CoerceValueCallback(CoerceTemp)),
+                    new ValidateValueCallback(ValidateTemp));
+        }
+
+        private static bool ValidateTemp(object value)
+        {
+            int v = (int)value;
+            if (v >= -50 && v <= 50)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static object CoerceTemp(DependencyObject d, object baseValue)
+        {
+            int v = (int)baseValue;
+            if (v >= -50 && v <= 50)
+            {
+                return v;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
